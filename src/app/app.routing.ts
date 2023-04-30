f57f989219fd5ad8a1aed473a552c4473b0d99bc
Erica Xavier <ericaxav@gmail.com>
2023-04-29 23:20:47 -0300

    Adicionando Componentes e Rotas

---
+++ src/app/app.routing.ts
@@ -0,0 +1,24 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./components/home/login/login.component";
+import { RegisterComponent } from "./components/home/register/register.component";
+import { PasswordComponent } from "./components/home/password/password.component";
+
+/* 
+    Mapeamento de rota para os componentes.
+*/
+
+const routes : Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'acessar-conta' }, //rota raiz do projeto
+    { path : 'acessar-conta', component: LoginComponent },
+    { path : 'criar-conta', component: RegisterComponent },
+    { path : 'esqueci-minha-senha', component: PasswordComponent }
+
+];
+
+@NgModule({
+    imports: [ RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
