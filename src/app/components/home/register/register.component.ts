20c96e05adf4e4b7f08bd30c2c24c1558112570c
Erica Xavier <ericaxav@gmail.com>
2023-05-02 22:51:01 -0300

    Formulário e validações

--- src/app/components/home/register/register.component.ts
+++ src/app/components/home/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  formRegister = new FormGroup({
+    nome: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[a-zA-ZÀ-Üà-ü\s]{8,100}$/)
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    senha: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*_+\-=])[a-zA-Z\d!@#$%&*_+\-=]{8,}$/)
+    ]),
+    senhaConfirmacao: new FormControl('', [
+      Validators.required
+    ])
+
+  });
+
+  // exibindo erros de validaçao no formulário
+
+  get form(): any{
+    return this.formRegister.controls;
+  }
+
+  // capturando dados do formulário
+
+  onSubmit() : void {
+    console.log(this.formRegister.value);
+ }
 
 }
